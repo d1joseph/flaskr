bbd7a0e02aefafe0f5124ae61b107413bda29aee
d1joseph <dhiv.joseph@gmail.com>
2021-04-20 12:47:11 +1000

    finished unit testing modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from flaskr.db import get_db, init_db
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
 
+
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
